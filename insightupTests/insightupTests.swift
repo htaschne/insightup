a86ec3670ac1af71b453dfcd46ae933b18b0865b
Agatha Schneider <agatha.schneider@outlook.com>
2025-05-12 18:26:57 -0300

    Initial Commit

---
+++ insightupTests/insightupTests.swift
@@ -0,0 +1,17 @@
+//
+//  insightupTests.swift
+//  insightupTests
+//
+//  Created by Agatha Schneider on 12/05/25.
+//
+
+import Testing
+@testable import insightup
+
+struct insightupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
