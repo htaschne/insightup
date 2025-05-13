207705c19b01ce3295281c07e1a4fba1fd1a1277
Agatha Schneider <agatha.schneider@outlook.com>
2025-05-13 08:43:35 -0300

    remove storyboard and setup viewcode

--- insightup/ViewController.swift
+++ insightup/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import DesignSystem
+import SwiftUICore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(Color.DesignSystem.gray6Dark)
     }
 
 
